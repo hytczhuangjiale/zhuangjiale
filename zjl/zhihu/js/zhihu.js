e28bad571d803fad0a72c7ff98b3da40103e494e
Adjoy <449266578@qq.com>
2014-08-15 11:39:52 +0800

    企业实训-04
    知乎js KFC框架

--- zjl/zhihu/js/zhihu.js
+++ zjl/zhihu/js/zhihu.js
@@ -1,9 +1,29 @@
-//ul标签
+
 $(function(){
+//Login登录注册
+$(".login").click(function(){
+	$(this).parent().parent().animate({"top":"0px","left":"400px"},600);
+	$(this).parent().parent().next().animate({"top":"0px","left":"140px"},600);
+	return false;
+	//  $(this).parent().parent().animate({"top":"0px","left":"400px"},300);
+	// $(this).parent().parent().next().animate({"top":"0px","left":"140px"},300);
+});
+ $(document).click(function(){
+  	$(".tpspecial1").animate({"top":"0px","left":"140px"},600);
+    $(".tpspecial2").animate({"top":"0px","left":"400px"},600);
+  });
+$(".zhuce").click(function(){
+	$(this).parent().parent().animate({"top":"0px","left":"400px"},600);
+	$(this).parent().parent().prev().animate({"top":"0px","left":"140px"},600);
+	return false;
+ });
+
+//ul标签
 $(".logo").hover(function(){
 	number=$(this).attr("logoindex");
     showimageul();
 });
+
 
 });
 function showimageul(){
